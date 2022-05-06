f0acd994137fc5c313d27cf147482dfffadbea7b
mybloom <baejungeun11@gmail.com>
2022-05-06 16:15:53 +0900

    chap6: 상품 service 개발

---
+++ src/main/java/com/lecture/jpausefirst/service/ItemService.java
@@ -0,0 +1,29 @@
+package com.lecture.jpausefirst.service;
+
+import com.lecture.jpausefirst.domain.item.Item;
+import com.lecture.jpausefirst.repository.ItemRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+	private final ItemRepository itemRepository;
+
+	@Transactional
+	public void saveItem(Item item) {
+		itemRepository.save(item);
+	}
+
+	public List<Item> findItems() {
+		return itemRepository.findAll();
+	}
+
+	public Item findOne(Long itemId) {
+		return itemRepository.findOne(itemId);
+	}
+}
