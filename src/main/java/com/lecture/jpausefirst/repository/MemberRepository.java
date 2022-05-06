161272c62f8950a676e981bcd1d791b808ca5d57
mybloom <baejungeun11@gmail.com>
2022-05-06 14:42:51 +0900

    chap5: @PersistenceContext 대신 @Autowired로 EntityManager의존성 주입
    - spring boot, spring Data JPA를 사용할 경우,
    - @PersistenceContext 대신 @Autowired로 사용할 수 있다.

--- src/main/java/com/lecture/jpausefirst/repository/MemberRepository.java
+++ src/main/java/com/lecture/jpausefirst/repository/MemberRepository.java
@@ -3,18 +3,16 @@ package com.lecture.jpausefirst.repository;
 import com.lecture.jpausefirst.domain.Member;
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository //ComponentScan에 의해 자동으로 스프링 빈으로 관리
+@RequiredArgsConstructor
 public class MemberRepository {
 
-	@PersistenceContext //스프링이 JPA의 엔티티매니저를 만들어서 주입해준다.
-	private EntityManager entityManager;
-
-	public MemberRepository(EntityManager entityManager) {
-		this.entityManager = entityManager;
-	}
+//	@PersistenceContext //스프링이 JPA의 엔티티매니저를 만들어서 주입해준다.
+	//spring boot, spring Data JPA를 사용할 경우, @PersistenceContext대신 @Autowired로 사용할 수 있다.
+	private final EntityManager entityManager;
 
 	public void save(Member member) {
 		entityManager.persist(member);
