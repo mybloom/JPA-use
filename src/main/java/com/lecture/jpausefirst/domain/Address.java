dc67176d031680f74438aae5f9a9233b292ba453
mybloom <baejungeun11@gmail.com>
2022-05-06 12:04:05 +0900

    chap3.docs: 테이블, 객체 설계 설명 추가

--- src/main/java/com/lecture/jpausefirst/domain/Address.java
+++ src/main/java/com/lecture/jpausefirst/domain/Address.java
@@ -1,10 +1,13 @@
 package com.lecture.jpausefirst.domain;
 
 import javax.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //JPA스펙상 엔티티를 객체로 생성하기 위한 기본생성자 제공
 public class Address {
 
 	private String city;
