35cd115acb75dc2422944d072481da169be5a67c
mybloom <baejungeun11@gmail.com>
2022-05-06 18:07:11 +0900

    chap7: 주문생성 테스트 작성 - 상품주문_재고수량초과 테스트

--- src/test/java/com/lecture/jpausefirst/service/OrderServiceTest.java
+++ src/test/java/com/lecture/jpausefirst/service/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package com.lecture.jpausefirst.service;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.lecture.jpausefirst.domain.Address;
@@ -7,6 +8,7 @@ import com.lecture.jpausefirst.domain.Member;
 import com.lecture.jpausefirst.domain.Order;
 import com.lecture.jpausefirst.domain.OrderStatus;
 import com.lecture.jpausefirst.domain.item.Book;
+import com.lecture.jpausefirst.exception.NotEnoughStockException;
 import com.lecture.jpausefirst.repository.OrderRepository;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
@@ -31,16 +33,8 @@ class OrderServiceTest {
 	@Test
 	void 상품주문() {
 		//given
-		Member member = new Member();
-		member.setName("회원1");
-		member.setAddress(new Address("서울", "강남구", "123-123"));
-		entityManager.persist(member);
-
-		Book book = new Book();
-		book.setName("JPA");
-		book.setPrice(39000);
-		book.setStockQuantity(10);
-		entityManager.persist(book);
+		Member member = createMember();
+		Book book = createBook("JPA", 39000, 10);
 
 		int orderCount = 2;
 
@@ -52,8 +46,22 @@ class OrderServiceTest {
 
 		assertEquals(OrderStatus.ORDER, getOrder.getStatus());
 		assertEquals(1, getOrder.getOrderItems().size());
-		assertEquals(39000 * orderCount, getOrder.getTotalPrice());;
+		assertEquals(39000 * orderCount, getOrder.getTotalPrice());
+		;
 		assertEquals(8, book.getStockQuantity());
+	}
+
+	@Test
+	void 상품주문_재고수량초과() {
+		//given
+		Member member = createMember();
+		Book book = createBook("JPA", 39000, 10);
+
+		int orderCount = 11;
+
+		assertThatThrownBy(() -> orderService.order(member.getId(), book.getId(), orderCount))
+			.isInstanceOf(NotEnoughStockException.class);
+
 	}
 
 	@Test
@@ -66,13 +74,23 @@ class OrderServiceTest {
 
 	}
 
-	@Test
-	void 상품주문_재고수량초과() {
-		//given
 
-		//when
 
-		//then
 
+	private Book createBook(String name, int price, int stockQuantity) {
+		Book book = new Book();
+		book.setName(name);
+		book.setPrice(price);
+		book.setStockQuantity(stockQuantity);
+		entityManager.persist(book);
+		return book;
+	}
+
+	private Member createMember() {
+		Member member = new Member();
+		member.setName("회원1");
+		member.setAddress(new Address("서울", "강남구", "123-123"));
+		entityManager.persist(member);
+		return member;
 	}
 }
