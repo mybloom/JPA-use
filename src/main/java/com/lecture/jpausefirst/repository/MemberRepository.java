e00d2e959079afdd69230c216d2607483ed72746
mybloom <baejungeun11@gmail.com>
2022-05-06 14:10:00 +0900

    chap5: 회원 도메인 개발 - MemberRepository 개발

---
+++ src/main/java/com/lecture/jpausefirst/repository/MemberRepository.java
@@ -0,0 +1,33 @@
+package com.lecture.jpausefirst.repository;
+
+import com.lecture.jpausefirst.domain.Member;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository //ComponentScan에 의해 자동으로 스프링 빈으로 관리
+public class MemberRepository {
+
+	@PersistenceContext //스프링이 JPA의 엔티티매니저를 만들어서 주입해준다.
+	private EntityManager entityManager;
+
+	public void save(Member member) {
+		entityManager.persist(member);
+	}
+
+	public Member findOne(Long id) {
+		return entityManager.find(Member.class, id);
+	}
+
+	public List<Member> findAll() {
+		return entityManager.createQuery("select m from Member m", Member.class)
+			.getResultList();
+	}
+
+	public List<Member> findByNAme(String name) {
+		return entityManager.createQuery("select m from Member m where m.name = : name", Member.class)
+			.setParameter("name", name)
+			.getResultList();
+	}
+}
