489ee1da23684b2c2eea51fb0c199162c50ed1d2
mybloom <baejungeun11@gmail.com>
2022-05-08 00:00:42 +0900

    chap1: 회원 등록 API 개발 - V2 리팩토링
    - REQUEST용 DTO를 만들어서 API 스펙과 도메인을 분리
    - validation 처리를 DTO에서 처리

--- src/main/java/com/lecture/jpausefirst/api/MemberApiController.java
+++ src/main/java/com/lecture/jpausefirst/api/MemberApiController.java
@@ -25,6 +25,20 @@ public class MemberApiController {
 
 	}
 
+	@PostMapping("/api/v2/members")
+	public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+		Member member = new Member();
+		member.setName(request.getName());
+
+		Long id = memberService.join(member);
+		return new CreateMemberResponse(id);
+	}
+
+	@Data
+	static class CreateMemberRequest {
+		private String name;
+	}
+
 	@Data
 	@AllArgsConstructor(access = AccessLevel.PRIVATE)
 	static class CreateMemberResponse {
